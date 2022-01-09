d5ed2ecbab7c48b1e80498c6e6191e9b132e1cb5
F1uk369 <96375953+F1uk369@users.noreply.github.com>
2022-01-09 06:54:08 +0000

    Update 2-user.sh

--- 2-user.sh
+++ 2-user.sh
@@ -11,13 +11,13 @@ EOF
 echo -ne "
 ------------------------------------------------------------------------------
                     Automated Arch Linux Installer
-                        SCRIPTHOME: ArchTitus
+                        SCRIPTHOME: easyarch
 ------------------------------------------------------------------------------
 
 Installing AUR Softwares
 "
 # You can solve users running this script as root with this and then doing the same for the next for statement. However I will leave this up to you.
-source $HOME/ArchTitus/setup.conf
+source $HOME/easyarch/setup.conf
 
 cd ~
 git clone "https://aur.archlinux.org/yay.git"
@@ -29,12 +29,12 @@ git clone "https://github.com/ChrisTitusTech/zsh"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 ln -s "~/zsh/.zshrc" ~/.zshrc
 
-yay -S --noconfirm --needed - < ~/ArchTitus/pkg-files/aur-pkgs.txt
+yay -S --noconfirm --needed - < ~/easyarch/pkg-files/aur-pkgs.txt
 
 export PATH=$PATH:~/.local/bin
-cp -r ~/ArchTitus/dotfiles/* ~/.config/
+cp -r ~/easyarch/dotfiles/* ~/.config/
 pip install konsave
-konsave -i ~/ArchTitus/kde.knsv
+konsave -i ~/easyarch/kde.knsv
 sleep 1
 konsave -a kde
 
