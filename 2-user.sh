98d055680df2c04250e9bd371420d3a927f07e0e
F1uk369 <96375953+F1uk369@users.noreply.github.com>
2022-01-09 06:00:05 +0000

    Add files via upload

---
+++ 2-user.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+echo "-------------------------------------------------------------------------"
+cat << "EOF"
+  _____                       _                _
+ | ____| __ _  ___  _   _    / \    _ __  ___ | |__
+ |  _|  / _` |/ __|| | | |  / _ \  | '__|/ __|| '_ \
+ | |___| (_| |\__ \| |_| | / ___ \ | |  | (__ | | | |
+ |_____|\__,_||___/ \__, |/_/   \_\|_|   \___||_| |_|
+                    |___/
+EOF
+echo -ne "
+------------------------------------------------------------------------------
+                    Automated Arch Linux Installer
+                        SCRIPTHOME: ArchTitus
+------------------------------------------------------------------------------
+
+Installing AUR Softwares
+"
+# You can solve users running this script as root with this and then doing the same for the next for statement. However I will leave this up to you.
+source $HOME/ArchTitus/setup.conf
+
+cd ~
+git clone "https://aur.archlinux.org/yay.git"
+cd ~/yay
+makepkg -si --noconfirm
+cd ~
+touch "~/.cache/zshhistory"
+git clone "https://github.com/ChrisTitusTech/zsh"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+ln -s "~/zsh/.zshrc" ~/.zshrc
+
+yay -S --noconfirm --needed - < ~/ArchTitus/pkg-files/aur-pkgs.txt
+
+export PATH=$PATH:~/.local/bin
+cp -r ~/ArchTitus/dotfiles/* ~/.config/
+pip install konsave
+konsave -i ~/ArchTitus/kde.knsv
+sleep 1
+konsave -a kde
+
+echo -ne "
+-------------------------------------------------------------------------
+                    SYSTEM READY FOR 3-post-setup.sh
+-------------------------------------------------------------------------
+"
+exit
---
+++ archtitus.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Find the name of the folder the scripts are in
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+echo "-------------------------------------------------------------------------"
+cat << "EOF"
+  _____                       _                _
+ | ____| __ _  ___  _   _    / \    _ __  ___ | |__
+ |  _|  / _` |/ __|| | | |  / _ \  | '__|/ __|| '_ \
+ | |___| (_| |\__ \| |_| | / ___ \ | |  | (__ | | | |
+ |_____|\__,_||___/ \__, |/_/   \_\|_|   \___||_| |_|
+                    |___/
+EOF
+echo -ne "
+----------------------------------------------------------------------------------------
+                            Automated Arch Linux Installer
+----------------------------------------------------------------------------------------
+                        Scripts are in directory named ArchTitus
+"
+    bash startup.sh
+    source $SCRIPT_DIR/setup.conf
+    bash 0-preinstall.sh
+    arch-chroot /mnt /root/ArchTitus/1-setup.sh
+    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/ArchTitus/2-user.sh
+    arch-chroot /mnt /root/ArchTitus/3-post-setup.sh
+
+echo "-------------------------------------------------------------------------"
+cat << "EOF"
+  _____                       _                _
+ | ____| __ _  ___  _   _    / \    _ __  ___ | |__
+ |  _|  / _` |/ __|| | | |  / _ \  | '__|/ __|| '_ \
+ | |___| (_| |\__ \| |_| | / ___ \ | |  | (__ | | | |
+ |_____|\__,_||___/ \__, |/_/   \_\|_|   \___||_| |_|
+                    |___/
+EOF
+echo -ne "
+-------------------------------------------------------------------------
+                    Automated Arch Linux Installer
+-------------------------------------------------------------------------
+                Done - Please Eject Install Media and Reboot
+"
